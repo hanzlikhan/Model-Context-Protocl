84385f079f8dcd646c1948cc263e43014c356065
Muhammad Hanzla <146532605+hanzlikhan@users.noreply.github.com>
2026-01-13 23:33:06 +0500

    commit

---
+++ app.java
@@ -0,0 +1,13 @@
+package com.example.myapplication;
+public class Click extends AppCompatActivity {
+    Button btnno1;
+    protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_click);
+        btnno1 = findViewById(R.id.button_send1);
+        android:id="@+id/button_send1"
+        btnno1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(click.this, Signin.class);
+                startActivity(intent);
+            )}};
+        
